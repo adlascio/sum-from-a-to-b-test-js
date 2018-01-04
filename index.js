ce1ff3e9493e4698dc248c3b9691b2aea9163bf2
Arthur Di Lascio <arthur.dilascio@gmail.com>
2018-01-04 14:39:48 -0800

    exercise done

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
+  if(toN >= fromN){
+    if (fromN === toN) {
+      return fromN;
+    }
+    return fromN + sum(fromN + 1, toN);
+  }
+  
 }
 
 module.exports = sum;
